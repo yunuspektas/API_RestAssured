7eab36347f5c0758375ada9c564164b7c0bf59b3
Yunus Pektas <yunuspektas@protonmail.com>
2022-09-12 13:14:13 +0300

    adding comments

--- src/test/java/GetRequest01.java
+++ src/test/java/GetRequest01.java
@@ -8,9 +8,9 @@ public class GetRequest01 {
     @Test
     public void get01(){
 
-        String url ="https://reqres.in/api/users/02";
-        Response response = given().when().get(url);
-        response.prettyPrint();
+        String url ="https://reqres.in/api/users/02"; // api end-point eklendi
+        Response response = given().when().get(url); // response gönderildi
+        response.prettyPrint(); // Json formatında dönen değer ekrana bastırılıyor
 
     }
 }
